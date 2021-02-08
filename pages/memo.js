efbe5d54552895c1830e9d4f24f2eb7d5ae5dca1
wengqianyi <452592978@qq.com>
2021-02-08 21:03:27 +0800

    feat: add a heart

--- pages/memo.js
+++ pages/memo.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from "../styles/memo.module.css";
 
 export default function Memo() {
   return (
@@ -7,8 +8,19 @@ export default function Memo() {
         <title>Memo</title>
       </Head>
 
-      <main>
-        <div className="p-6 w-40">A memo card.</div>
+      <main className="box-border bg-bgWhite h-screen flex-1 flex items-center">
+        <div class="container rounded-full sm:w-3/5 md:w-2/5 lg:w-1/5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="heart"
+            viewBox="0 0 362 327"
+          >
+            <path
+              d="M348.82 107.15c0-51.97-42.13-94.1-94.1-94.1-29.82 0-56.39 13.87-73.63 35.51l-.77-.77-.01.01c-17.25-21.2-43.55-34.75-73.02-34.75-51.97 0-94.1 42.13-94.1 94.1 0 29.47 13.55 55.77 34.75 73.02l-.69.69 133.08 133.08L313.4 180.87l-.09-.09c21.64-17.24 35.51-43.81 35.51-73.63z"
+              fill="#e22d48"
+            />
+          </svg>
+        </div>
       </main>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 module.exports = {
   purge: [
     // Use *.tsx if using TypeScript
-    './pages/**/*.js',
-    './components/**/*.js'
-  ]
+    "./pages/**/*.js",
+    "./components/**/*.js",
+  ],
+  theme: {
+    colors: {
+      bgWhite: "#e0e5ec",
+      heartRed: "#e22d48",
+    },
+    fontFamily: {
+      sans: ["Arial", "Microsoft YaHei", "黑体", "宋体", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "2rem",
+        sm: "1rem",
+        md: "2rem",
+        lg: "2rem",
+        xl: "3rem",
+        "2xl": "3rem",
+      },
+    },
+  },
   // ...
-}
+};
