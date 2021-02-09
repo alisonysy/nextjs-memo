cc159e163ebf6a7e32c145eb31c4a724e9bfb1fe
wengqianyi <452592978@qq.com>
2021-02-09 14:43:01 +0800

    feat: init leancloud

--- pages/memo.js
+++ pages/memo.js
@@ -1,10 +1,22 @@
 import Head from "next/head";
 import styles from "../styles/memo.module.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import lcKey from "../lcKey.json";
+const AV = require("leancloud-storage");
+
+AV.init({
+  appId: lcKey.appId,
+  appKey: lcKey.appKey,
+  serverURL: lcKey.serverURL,
+});
+// const { Query, User } = AV;
+
 
 export default function Memo() {
   const heartRef = useRef(null);
   const heartPath = useRef(null);
+
+  const [showCards, setShowCards] = useState(false);
 
   function onHeartClick() {
     if (heartRef.current) {
@@ -12,6 +24,11 @@ export default function Memo() {
       heartRef.current.classList.add("shadow-pressed");
       heartPath.current.setAttribute("fill", "#e22d48");
       heartPath.current.setAttribute("stroke", "none");
+      heartRef.current.classList.add("animate-ping");
+      setTimeout(() => {
+        heartRef.current.classList.remove("animate-ping");
+        setShowCards(true);
+      }, 600);
     }
   }
 
@@ -21,9 +38,9 @@ export default function Memo() {
         <title>Memo</title>
       </Head>
 
-      <main className="box-border bg-bgWhite h-screen flex-1 flex items-center">
+      <main className="p-4 box-border bg-bgWhite h-screen flex-1 flex items-center justify-center flex-col">
         <div
-          className="container rounded-full w-20 lg:w-24 h-20 lg:h-24 shadow-neu flex items-center cursor-pointer"
+          className="container rounded-full w-20 lg:w-24 h-20 lg:h-24 shadow-neu flex items-center cursor-pointer transition-all"
           onClick={onHeartClick}
           ref={heartRef}
         >
@@ -36,12 +53,13 @@ export default function Memo() {
               d="M348.82 107.15c0-51.97-42.13-94.1-94.1-94.1-29.82 0-56.39 13.87-73.63 35.51l-.77-.77-.01.01c-17.25-21.2-43.55-34.75-73.02-34.75-51.97 0-94.1 42.13-94.1 94.1 0 29.47 13.55 55.77 34.75 73.02l-.69.69 133.08 133.08L313.4 180.87l-.09-.09c21.64-17.24 35.51-43.81 35.51-73.63z"
               fill="none"
               stroke="#a3b1c6"
-              stroke-width={10}
+              strokeWidth={10}
               ref={heartPath}
               className="transition-colors"
             />
           </svg>
         </div>
+        {showCards && <div className="h-screen">many items...</div>}
       </main>
     </>
   );
